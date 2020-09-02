89b98462b8908dd3e4f357b51da512d89a7905f0
dawplus <dawschae@gmail.com>
2020-09-02 16:52:35 +0900

    menu

--- exp/src/App/layout/Sidebar/index.js
+++ exp/src/App/layout/Sidebar/index.js
@@ -45,26 +45,8 @@ const SideBar = () => {
                             </li>
                             <li>
                                 <Link to="/exception">
-                                    <i className="metismenu-icon pe-7s-rocket"></i>
+                                    <i className="metismenu-icon pe-7s-display2"></i>
                                     Exception 관리 
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/map">
-                                    <i className="metismenu-icon pe-7s-rocket"></i>
-                                    지도 관리 
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/guide">
-                                    <i className="metismenu-icon pe-7s-rocket"></i>
-                                    가이드 관리 
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/question">
-                                    <i className="metismenu-icon pe-7s-rocket"></i>
-                                    문제 관리 
                                 </Link>
                             </li>
                             <li>
@@ -73,173 +55,33 @@ const SideBar = () => {
                                     팀 관리 
                                 </Link>
                             </li>
-                            <li className="app-sidebar__heading">UI Components</li>
                             <li>
                                 <a href="index.html">
                                     <i className="metismenu-icon pe-7s-diamond"></i>
-                                    Elements
+                                    문제관리
                                     <i className="metismenu-state-icon pe-7s-angle-down caret-left"></i>
                                 </a>
                                 <ul>
                                     <li>
-                                        <a href="elements-buttons-standard.html">
-                                            <i className="metismenu-icon"></i>
-                                            Buttons
-                                        </a>
+                                        <Link to="/question">
+                                            <i className="metismenu-icon pe-7s-rocket"></i>
+                                            문제 관리 
+                                        </Link>
                                     </li>
                                     <li>
-                                        <a href="elements-dropdowns.html">
-                                            <i className="metismenu-icon">
-                                            </i>Dropdowns
-                                        </a>
+                                        <Link to="/map">
+                                            <i className="metismenu-icon pe-7s-rocket"></i>
+                                            지도 관리 
+                                        </Link>
                                     </li>
                                     <li>
-                                        <a href="elements-icons.html">
-                                            <i className="metismenu-icon">
-                                            </i>Icons
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-badges-labels.html">
-                                            <i className="metismenu-icon">
-                                            </i>Badges
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-cards.html">
-                                            <i className="metismenu-icon">
-                                            </i>Cards
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-list-group.html">
-                                            <i className="metismenu-icon">
-                                            </i>List Groups
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-navigation.html">
-                                            <i className="metismenu-icon">
-                                            </i>Navigation Menus
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="elements-utilities.html">
-                                            <i className="metismenu-icon">
-                                            </i>Utilities
-                                        </a>
+                                        <Link to="/guide">
+                                            <i className="metismenu-icon pe-7s-rocket"></i>
+                                            가이드 관리 
+                                        </Link>
                                     </li>
                                 </ul>
                             </li>
-                            <li>
-                                <a href="index.html">
-                                    <i className="metismenu-icon pe-7s-car"></i>
-                                    Components
-                                    <i className="metismenu-state-icon pe-7s-angle-down caret-left"></i>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="components-tabs.html">
-                                            <i className="metismenu-icon">
-                                            </i>Tabs
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-accordions.html">
-                                            <i className="metismenu-icon">
-                                            </i>Accordions
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-notifications.html">
-                                            <i className="metismenu-icon">
-                                            </i>Notifications
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-modals.html">
-                                            <i className="metismenu-icon">
-                                            </i>Modals
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-progress-bar.html">
-                                            <i className="metismenu-icon">
-                                            </i>Progress Bar
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-tooltips-popovers.html">
-                                            <i className="metismenu-icon">
-                                            </i>Tooltips &amp; Popovers
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-carousel.html">
-                                            <i className="metismenu-icon">
-                                            </i>Carousel
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-calendar.html">
-                                            <i className="metismenu-icon">
-                                            </i>Calendar
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-pagination.html">
-                                            <i className="metismenu-icon">
-                                            </i>Pagination
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-scrollable-elements.html">
-                                            <i className="metismenu-icon">
-                                            </i>Scrollable
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="components-maps.html">
-                                            <i className="metismenu-icon">
-                                            </i>Maps
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li  >
-                                <a href="tables-regular.html">
-                                    <i className="metismenu-icon pe-7s-display2"></i>
-                                    Tables
-                                </a>
-                            </li>
-                            <li className="app-sidebar__heading">Widgets</li>
-                            <li>
-                                <a href="dashboard-boxes.html">
-                                    <i className="metismenu-icon pe-7s-display2"></i>
-                                    Dashboard Boxes
-                                </a>
-                            </li>
-                            <li className="app-sidebar__heading">Forms</li>
-                            <li>
-                                <a href="forms-controls.html">
-                                    <i className="metismenu-icon pe-7s-mouse">
-                                    </i>Forms Controls
-                                </a>
-                            </li>
-                            <li>
-                                <a href="forms-layouts.html">
-                                    <i className="metismenu-icon pe-7s-eyedropper">
-                                    </i>Forms Layouts
-                                </a>
-                            </li>
-                            <li>
-                                <a href="forms-validation.html">
-                                    <i className="metismenu-icon pe-7s-pendrive">
-                                    </i>Forms Validation
-                                </a>
-                            </li>
-                          
-                            
                         </ul>
                     </div>
                 </div>
